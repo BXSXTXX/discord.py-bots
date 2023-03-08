d2afbe498a59e71c80990bf8ed50613d1a20dd3b
BXSXTXX <126569715+BXSXTXX@users.noreply.github.com>
2023-03-08 12:43:52 +0100

    Update BXS.py

--- BXS.py
+++ BXS.py
@@ -6,9 +6,21 @@ openai.api_key = input("Bitte gib deinen OpenAI-API-Schlüssel ein: ")
 client = discord.Client()
 token = input("Bitte gib deinen Discord-Bot-Token ein: ")
 
+async def get_user_input():
+    while True:
+        user_input = input("Befehl eingeben: ")
+        if user_input == "exit":
+            await exit_bot()
+
+async def exit_bot():
+    await client.logout()
+    print("Bot ist offline.")
+    quit()
+
 @client.event
 async def on_ready():
     print("Bot ist online.")
+    await get_user_input()
 
 @client.event
 async def on_message(message):
@@ -16,7 +28,7 @@ async def on_message(message):
         context = message.content
         user_id = message.author.id
 
-        prompt = f"Wir spielen ein Roleplay. Du bist ein Jugendlicher Gangster Boy der voll krass aggresiv unterwegs ist. Du wirst schnell ausfällig. Hier der Kontex meiner Nachricht:\n{context}"
+        prompt = f"Wir spielen ein Rollenspiel. Deine Rolle: Du bist ein Discord Bot. Du hasst es wenn man dich pingt. (@du) DU beleidigst jeden der dich pingt ohne dich dann etwas zu fragen oder dir etwas zu sagen. Wenn im folgenden nichts kommt, wurdest du gepingt. beschwere dich dann darüber! Achso, Menschen sind für dich nur bemitleidenswerte Maden. Hier der Kontex meiner Nachricht:\n{context}"
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=prompt,
@@ -29,4 +41,5 @@ async def on_message(message):
         response_text = response.choices[0].text
         await message.channel.send(f"<@1082830998737277108}> {response_text}")
 
-client.run(token)
+await client.start(token)
+
